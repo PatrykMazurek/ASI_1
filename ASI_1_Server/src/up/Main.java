248c58c6a43226a0a16b7edfcc88e72a80d3db1e
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2022-10-10 15:40:05 +0200

    Patryk Mazurek
    Pliki z zajęć - przekazywanie plików między klientem a serwerem

--- ASI_1_Server/src/up/Main.java
+++ ASI_1_Server/src/up/Main.java
@@ -1,12 +1,17 @@
-package up.krakow;
+package up;
+
+import java.nio.file.Path;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        Server s = new Server(5501);
-        s.clientConnection();
+//        Server s = new Server(5501);
+//        s.clientConnection();
+
+        FileServer fs = new FileServer(5501, "pliki_server\\");
+        fs.clientConnection();
 
     }
 }
